6fa7300d1b353ba714898f2c4fe56abf8cde7f2d
RubiiiT <rubenmulerocrespo@gmail.com>
2025-04-03 20:41:07 +0200

    primer commit creando carpetas front y back

---
+++ front-end/proyectoAppJuegos/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <main>
+        <h1>Eu bata</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCMmdKXI1TCsCPNsnNOj6COOaHLa9WpzNF7Q&s" alt="" />
+      </main>
+
+      
+    </>
+  )
+}
+
+export default App
